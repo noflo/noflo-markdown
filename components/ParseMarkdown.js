6dd9c03a1dee150fb70d338ed64a3df34c47cb61
Henri Bergius <henri.bergius@iki.fi>
2021-01-06 12:05:04 +0100

    Make linter happy

--- components/ParseMarkdown.js
+++ components/ParseMarkdown.js
@@ -1,45 +1,28 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const noflo = require('noflo');
 const fromMarkdown = require('commonmark');
 
-exports.getComponent = function() {
-  const c = new noflo.Component;
+exports.getComponent = () => {
+  const c = new noflo.Component();
   c.description = 'Convert Markdown to HTML';
   c.inPorts.add('in', {
     datatype: 'string',
-    description: 'Markdown source'
-  }
-  );
-  c.inPorts.add('gfm', {
-    datatype: 'boolean',
-    description: 'Use GitHub-flavored Markdown',
-    default: false,
-    control: true
-  }
-  );
-  c.outPorts.add('out',
-    {datatype: 'string'});
+    description: 'Markdown source',
+  });
+  c.outPorts.add('out', {
+    datatype: 'string',
+  });
   c.outPorts.add('error', {
     datatype: 'object',
-    required: false
-  }
-  );
+    required: false,
+  });
 
-  return c.process(function(input, output) {
+  return c.process((input, output) => {
     if (!input.hasData('in')) { return; }
-    if (input.attached('gfm').length && !input.hasData('gfm')) { return; }
     const data = input.getData('in');
 
-    const gfm = input.has('gfm') ? input.getData('gfm') : false;
-
-    const reader = new fromMarkdown.Parser;
-    const renderer = new fromMarkdown.HtmlRenderer;
+    const reader = new fromMarkdown.Parser();
+    const renderer = new fromMarkdown.HtmlRenderer();
     const ast = reader.parse(data);
-    return output.sendDone({
-      out: renderer.render(ast)});
+    output.sendDone({ out: renderer.render(ast) });
   });
 };
--- components/ToMarkdown.js
+++ components/ToMarkdown.js
@@ -1,43 +1,28 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
 const noflo = require('noflo');
 const toMarkdown = require('html2commonmark');
 
-exports.getComponent = function() {
-  const c = new noflo.Component;
+exports.getComponent = () => {
+  const c = new noflo.Component();
   c.description = 'Convert HTML to Markdown';
 
   c.inPorts.add('in', {
     datatype: 'string',
-    description: 'HTML source'
-  }
-  );
-  c.inPorts.add('tags', {
-    datatype: 'boolean',
-    description: 'Whether to allow HTML tags inside Markdown',
-    default: true,
-    control: true
-  }
-  );
-  c.outPorts.add('out',
-    {datatype: 'string'});
+    description: 'HTML source',
+  });
+  c.outPorts.add('out', {
+    datatype: 'string',
+  });
 
-  return c.process(function(input, output) {
-    let converter;
+  return c.process((input, output) => {
     if (!input.hasData('in')) { return; }
-    if (input.attached('tags').length && !input.hasData('tags')) { return; }
     const data = input.getData('in');
-
-    const tags = input.has('tags') ? input.getData('tags') : true;
 
     const converterOptions = {
       rawHtmlElements: ['iframe', 'article', 'video', 'table'],
-      interpretUnknownHtml: false
+      interpretUnknownHtml: false,
     };
 
+    let converter;
     if (noflo.isBrowser()) {
       converter = new toMarkdown.BrowserConverter(converterOptions);
     } else {
@@ -47,7 +32,8 @@ exports.getComponent = function() {
     const ast = converter.convert(data);
     const markdown = renderer.render(ast);
 
-    return output.sendDone({
-      out: markdown});
+    output.sendDone({
+      out: markdown,
+    });
   });
 };
